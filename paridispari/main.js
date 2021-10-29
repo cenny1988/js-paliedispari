ef2660e95ecec21c6b2c19f7ff775e367233e636
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-29 15:50:35 +0200

    input utente e numer random pc creati

--- paridispari/main.js
+++ paridispari/main.js
@@ -8,14 +8,22 @@ Dichiariamo chi ha vinto.
 */
 
 // Utente inserisce un numero da 1 a 5
-    // Controlliamo se il numero è nel range
+let userNum = parseInt(prompt('Inserisci un numero da 1 a 5: '));
 
 // Utente sceglie PARI o DISPARI
+let userChose = prompt('Scegli tra pari o dispari: ');
 
-// Generiamo e salviamo un numero random sempre da 1 a 5 per il PC ---> NB creare poi una FUNZIONE per generare num random!!!
+// Generiamo e salviamo un numero random sempre da 1 a 5 per il PC
+let pcNum = pariDispari();
+console.log(pcNum);
 
 // Sommiamo il numero del Utente e del PC
 
 // Controlliamo se il valore della somma ottenuta è pari o dispari ---> NB creare poi una FUNZIONE per questo!!!
 
 // Output Vincitore.
+
+// FUNZIONI
+function pariDispari(){
+    return x = Math.floor(Math.random() * 5) + 1;
+}
