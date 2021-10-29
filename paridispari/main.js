81bd5929aad37a1dcf7ba993239d672c423e1ff8
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-29 15:28:37 +0200

    pseudocodice es. paridispari creato

--- paridispari/main.js
+++ paridispari/main.js
@@ -6,3 +6,16 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+// Utente inserisce un numero da 1 a 5
+    // Controlliamo se il numero è nel range
+
+// Utente sceglie PARI o DISPARI
+
+// Generiamo e salviamo un numero random sempre da 1 a 5 per il PC ---> NB creare poi una FUNZIONE per generare num random!!!
+
+// Sommiamo il numero del Utente e del PC
+
+// Controlliamo se il valore della somma ottenuta è pari o dispari ---> NB creare poi una FUNZIONE per questo!!!
+
+// Output Vincitore.
