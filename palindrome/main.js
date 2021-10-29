dcc1590a9d1f056410aeca89d1433580b31cf707
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-29 14:21:12 +0200

    macro-scomposizine passaggi

--- palindrome/main.js
+++ palindrome/main.js
@@ -5,3 +5,15 @@ Creare una funzione per capire se la parola inserita è palindroma.
 Dare l’output relativo.
 */
 
+//Chiedere all’utente di inserire una parola
+
+
+//Creare una funzione per capire se la parola inserita è palindroma.
+    //scomporre la parola in lettere. nb --> possiamo creare un funzione
+
+    //copiare le lettere dalla parola in ordine inverso in un altro array x esempio.
+
+    //confrontare i valori dei singoli elementi dei 2 array e vedere se sono tutti uguali.
+
+
+//output risultato
