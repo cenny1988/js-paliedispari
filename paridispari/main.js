434a05ff80a6d67d79f0a772d95f0f4a38da6ade
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-10-29 15:56:43 +0200

    debug console.log corretti

--- paridispari/main.js
+++ paridispari/main.js
@@ -9,16 +9,19 @@ Dichiariamo chi ha vinto.
 
 // Utente inserisce un numero da 1 a 5
 let userNum = parseInt(prompt('Inserisci un numero da 1 a 5: '));
+console.log('Il numero scelto user è: ',userNum);
 
 // Utente sceglie PARI o DISPARI
 let userChose = prompt('Scegli tra pari o dispari: ');
+console.log(`hai scelto ${userChose}!`);
 
 // Generiamo e salviamo un numero random sempre da 1 a 5 per il PC
 let pcNum = numRandom();
-console.log(pcNum);
+console.log('Il numero del pc è: ',pcNum);
 
 // Sommiamo il numero del Utente e del PC
-let sumTot = somma();
+let sumTot = somma(userNum, pcNum);
+console.log('La somma totale è: ',sumTot);
 
 // Controlliamo se il valore della somma ottenuta è pari o dispari ---> NB creare poi una FUNZIONE per questo!!!
 
@@ -28,4 +31,8 @@ let sumTot = somma();
 // funzione 
 function numRandom(){
     return x = Math.floor(Math.random() * 5) + 1;
+}
+
+function somma(x, y){
+    return sum = x + y;
 }
